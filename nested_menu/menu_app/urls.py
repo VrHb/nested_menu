18b29fabb941c8817de29aff4fdb080ae140dfd4
Victor Abdukarimov <viredmi@gmail.com>
2023-03-25 01:19:48 +0300

    Add more templates for test menu

--- nested_menu/menu_app/urls.py
+++ nested_menu/menu_app/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import HomeView
+from .views import HomeView, ItemsView, OrdersView
 
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('orders', OrdersView.as_view(), name='orders'),
+    path('items', ItemsView.as_view(), name='items'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- nested_menu/menu_app/views.py
+++ nested_menu/menu_app/views.py
@@ -4,5 +4,9 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     template_name = 'home.html'
 
+class ItemsView(TemplateView):
+    template_name = 'items.html'
 
+class OrdersView(TemplateView):
+    template_name = 'orders.html'
 
