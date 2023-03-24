cc2d46579a1c05b8ff897abbed1002702047c0ac
Victor Abdukarimov <viredmi@gmail.com>
2023-03-24 16:20:44 +0300

    Added new models

--- nested_menu/menu_app/models.py
+++ nested_menu/menu_app/models.py
@@ -2,11 +2,56 @@ from django.db import models
 
 
 class Menu(models.Model):
-    name = models.CharField(max_length=100)
-    parent = models.ForeignKey(
-        'self',
+    title = models.CharField('Название меню', max_length=50)
+    slug = models.SlugField(
+        'Часть меню для url',
+        max_length=250,
+        blank=True,
+        null=True
+        )
+    url = models.CharField(
+        'Имя url',
+        max_length=250,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+    
+    def __str__(self):
+        return self.title
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(
+        Menu,
+        related_name='items',
         blank=True,
         null=True,
-        related_name='children',
-        on_delete=models.DO_NOTHING,
+        on_delete=models.CASCADE
     )
+    parent = models.ForeignKey(
+        'self',
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(
+        'Название элемента',
+        max_length=100,
+    )
+    url = models.CharField(
+        'Ссылка',
+        max_length=250,
+        blank=True
+    )
+    
+    class Meta:
+        verbose_name = 'Элемент меню'
+        verbose_name_plural = 'Элементы меню'
+
+    def __str__(self):
+        return self.title
+
+
